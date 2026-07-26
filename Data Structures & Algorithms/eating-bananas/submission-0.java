469b425cbd1b0b689ea58425113481f7cfbb3419
Monish <monishbalaji249@gmail.com>
2026-07-26 12:11:37 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int minspeed=1;
+        int maxspeed=0;
+        for(int pile:piles){
+            maxspeed=Math.max(maxspeed,pile);
+        }
+        while(minspeed<maxspeed){
+            int mid=minspeed+(maxspeed-minspeed)/2;
+            if(koko(piles,h,mid)){
+                maxspeed=mid;
+            }
+            else{
+                minspeed=mid+1;
+            }
+        }
+        return minspeed;
+        
+    }
+    public boolean koko(int[] piles,int h,int speed){
+        int hour=0;
+        for(int pile:piles){
+            hour+=(int)Math.ceil((double)pile/speed);
+        }
+        return hour<=h;
+    }
+}
